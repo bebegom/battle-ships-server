3ba66a87cbf6efb22fb0f4f24a196632e96cef77
Alex <alexanderbergquist80@gmail.com>
2022-05-18 14:09:58 +0200

    randomize who starts and emit to client

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -11,71 +11,72 @@ let room = []
 
 const lobby = []
 
+/**
+ * Helpers
+ * 
+ */
+
+// randomize what player that starts
+const randomPlayerStart = (userId, opponent) => {
+	const randomNumber = (max) => {
+		return Math.floor(Math.random() * max)
+	}
+
+	if (randomNumber(11) <= 5) {
+		//player one start
+		// io.to(userId).emit("game:yourturn")
+		return userId 
+	} else {
+		//player two start
+		// io.to(opponent).emit("game:wait") 
+		return opponent
+	}
+}
+
 
 /**
  * Handle a user connecting
  *
  */
 const handleConnect = (userId) => {
-	// debug(userId)
-	debug(`Client ${userId} clicked submit :O`);
-
-	// Create player
-	const playerTemplate = {
-		id: userId,
-		ships: {
-			battleship: {
-				hp: 4
-			},
-			cruiser: {
-				hp: 3
-			},
-			submarine: {
-				hp: 2
-			},
-			destroyer: {
-				hp: 2
-			}
-		}
-	}
+	// debug(`Client ${userId} clicked submit :O`);
 
 	// Push new user to lobby
-	lobby.push(playerTemplate)
-	debug(room)
+	lobby.push(userId)
 
-	// Check if a game already is ongoing
-	if (!room.length) {
+	// Check if a game already is ongoing and another user is in lobby
+	if (!room.length && lobby.length === 2) {
 
-		// check if another user is in lobby
-		if (lobby.length === 2) {
-			
-			room.push(lobby[0], lobby[1])
+		// push first two players from lobby into gameroom
+		room.push(lobby[0], lobby[1])
 
-			const player1 = room[0];
-			const player2 = room[1];
+		// find opponent
+		const opponent = room.find(user => user = !this.id)
 
-			const activeUser = room.find(user => user = userId)
-			const opponent = room.find(user => user = !userId)
-
-			// emit to users in room
-			io.to(userId).emit('game:start', activeUser)
-
-
-			lobby.splice(0, 2);
-		}
+		// remove two first players from lobby
+		lobby.splice(0, 2);
 	
+		// determine who starts by random
+		const startingPlayer = randomPlayerStart(userId, opponent)
+		const secondPlayer = room.find(user => user = !startingPlayer)
+
+		// emit to players who starts
+		io.to(startingPlayer).emit("game:playerTurn")
+		io.to(secondPlayer).emit("game:playerWaiting")
 	} else {
 		// wait for ongoing game to end
+		debug('Wait for game.')
 	}
 
+	debug(room)
+}
 
-	// Start game
-
-	// If more than two players => push to lobby
-
-
-
-
+/**
+ * Handle room reset button
+ * (for dev, delete later)
+ */
+const handleResetRoom = () => {
+	room = [];
 }
 
 /**
@@ -97,11 +98,11 @@ module.exports = function(socket, _io) {
 
 	debug(`Client ${socket.id} connected :)`)
 
-	
-	socket.on('user:connect', handleConnect);
+	// listen to room reset (for dev, delete later) 
+	socket.on("reset:room", handleResetRoom);
 
-	// handle user connect
-	socket.on('connection', handleConnect)
+	// listen to user connect
+	socket.on('user:connect', handleConnect);
 
 	// handle user disconnect
 	socket.on('disconnect', handleDisconnect);
