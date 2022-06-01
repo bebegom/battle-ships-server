23e27c17d8bd928099984df64789e2b5c3e11bd4
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-06-01 20:41:36 +0200

    cleaned up

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,48 +1,42 @@
 /**
- * Socket Controller
- */
+* Socket Controller
+*/
 
-//  const { emit } = require('nodemon');
-
- const debug = require('debug')('clock:socket_controller');
- let io = null; // socket.io server instance
+const debug = require('debug')('clock:socket_controller');
+let io = null; // socket.io server instance
  
- let room = []
+let room = []
  
- const lobby = []
+const lobby = []
  
- /**
-  * Helpers
-  */
+/**
+* Helpers
+*/
  
- // randomize what player that starts
- const randomPlayerStart = (userId, opponent) => {
-	 const randomNumber = (max) => {
-		 return Math.floor(Math.random() * max)
-	 }
+// randomize what player that starts
+const randomPlayerStart = (userId, opponent) => {
+	const randomNumber = (max) => {
+		return Math.floor(Math.random() * max)
+	}
  
-	 if (randomNumber(11) <= 5) {
-		 //player one start
-		 return userId 
-	 } else {
-		 //player two start
-		 return opponent
-	 }
- }
+	if (randomNumber(11) <= 5) {
+		//player one start
+		return userId 
+	} else {
+		//player two start
+		return opponent
+	}
+}
  
  
- /**
-  * Handle a user connecting
-  */
- const handleConnect = function(userId) {
+/**
+* Handle a user connecting
+*/
+const handleConnect = function(userId) {
 
 	if (room.length >= 2) {
-
 		// wait for ongoing game to end
-		debug('There is already an ongoing game')
-
 		io.to(userId).emit("game:occupied")
-
 	} else {
 		// Push new user to lobby if length is less than 2
 		if(lobby.length >= 0 && lobby.length < 2 && room.length === 0) {
@@ -60,11 +54,9 @@
 
 				// determine who starts by random
 				const startingPlayer = randomPlayerStart(userId, opponent)
-				const secondPlayer = room.find(user => user != startingPlayer)
 		
 				// emit to players who starts
 				io.to(startingPlayer).emit("game:playerTurn", startingPlayer)
-				// io.to(secondPlayer).emit("game:playerWaiting")
 				
 				io.emit("game:start")
 			}
@@ -72,31 +64,22 @@
 	}
 }
  
- /**
-  * Handle room reset button
-  * (for dev, delete later)
-  */
- const handleResetRoom = (socketId) => {
-	// if (room.length > 1) {
-	// 	room = [];
-	// } 
-	// io.to(socketId).emit("reset:ships")
-
+/**
+* Handle room reset button
+*/
+const handleResetRoom = (socketId) => {
 	const opponent = room.find(user => user != socketId)
 	io.to(opponent).emit("reset:opponent:left:game")
 
 	room = []
- }
+}
  
- /**
-  * Handle a user disconnecting
-  */
+/**
+* Handle a user disconnecting
+*/
 const handleDisconnect = function() {
-	debug(`Client ${this.id} disconnected :(`);
-
 	// find socketId in room, if not do nothing
 	const disconnectedPlayer = room.find(user => user === this.id)
-	debug('player who disconnected: ', disconnectedPlayer)
 
 	// if player was in room
 	if(disconnectedPlayer !== undefined) {
@@ -106,79 +89,75 @@ const handleDisconnect = function() {
 	}
 }
  
- /**
-  * Handle a user click and hit/miss response
-  */
- const handleUserClickBox = function(socketId, boxId)  {
-	 debug("Användare klickade på en ruta")
+/**
+* Handle a user click and hit/miss response
+*/
+const handleUserClickBox = function(socketId, boxId)  {
+	// convert from opponent ID to ID (oa5 => a5)
+	const convBoxId = boxId.slice(1)
  
-	 // convert from opponent ID to ID (oa5 => a5)
-	 const convBoxId = boxId.slice(1)
- 
-	 // find opponent in room
-	 const opponent = room.find(user => user != socketId)
- 
-	 // emit question to opponent
-	 io.to(opponent).emit('user:hitormiss', socketId, convBoxId)
- }
- 
- const handleClickResponse = function(socketId, boxId, hit) {
-	 const opponent = room.find(user => user != socketId)
- 
-	 io.to(socketId).emit('response:hitormiss', socketId, boxId, hit)
-	 io.to(opponent).emit('opponentClick:respons', socketId, boxId, hit)
- }
- 
- const handleNextPlayer = function(socketId) {
-	 const opponent = room.find(user => user != socketId)
-	 io.to(opponent).emit('game:playerTurn')
- }
- 
- const handleSendShipSunk = (socketId) => {
-	 const opponent = room.find(user => user != socketId)
-	 io.to(socketId).emit('sending:ship:sunk:to:opponent')
-	 io.to(opponent).emit('your:ship:sunk', socketId)
- } 
+	// find opponent in room
+	const opponent = room.find(user => user != socketId)
 
- const handleNoShipsLeft = (socketId) => {
+	// emit question to opponent
+	io.to(opponent).emit('user:hitormiss', socketId, convBoxId)
+}
+ 
+const handleClickResponse = function(socketId, boxId, hit) {
+	const opponent = room.find(user => user != socketId)
+
+	io.to(socketId).emit('response:hitormiss', socketId, boxId, hit)
+	io.to(opponent).emit('opponentClick:respons', socketId, boxId, hit)
+}
+ 
+const handleNextPlayer = function(socketId) {
+	const opponent = room.find(user => user != socketId)
+	io.to(opponent).emit('game:playerTurn')
+}
+ 
+const handleSendShipSunk = (socketId) => {
+	const opponent = room.find(user => user != socketId)
+	io.to(socketId).emit('sending:ship:sunk:to:opponent')
+	io.to(opponent).emit('your:ship:sunk', socketId)
+}
+
+const handleNoShipsLeft = (socketId) => {
 	io.emit("reset:ships")
 	const opponent = room.find(user => user != socketId)
 	io.to(socketId).emit('opponent:have:no:ships:left')
 	io.to(opponent).emit('you:lost')
- }
+}
 
- const handleOpponentLeft = (socketId) => {
-	 io.to(socketId).emit('reload')
- }
+const handleOpponentLeft = (socketId) => {
+	io.to(socketId).emit('reload')
+}
  
- /**
-  * Export controller and attach handlers to events
-  */
- module.exports = function(socket, _io) {
-	 // save a reference to the socket.io server instance
-	 io = _io;
+/**
+* Export controller and attach handlers to events
+*/
+module.exports = function(socket, _io) {
+	// save a reference to the socket.io server instance
+	io = _io;
  
-	 debug(`Client ${socket.id} connected :)`)
- 
-	 // listen to room reset (for dev, delete later) 
+	// listen to room reset (for dev, delete later) 
 	socket.on("reset:room", handleResetRoom);
  
-	 // listen to user connect
-	 socket.on('user:connect', handleConnect);
+	// listen to user connect
+	socket.on('user:connect', handleConnect);
  
-	 // handle user disconnect
-	 socket.on('disconnect', handleDisconnect);
+	// handle user disconnect
+	socket.on('disconnect', handleDisconnect);
  
-	 // listen to user:click
-	 socket.on('user:click', handleUserClickBox)
+	// listen to user:click
+	socket.on('user:click', handleUserClickBox)
  
-	 socket.on('click:response', handleClickResponse)
+	socket.on('click:response', handleClickResponse)
  
-	 socket.on('game:nextPlayer', handleNextPlayer)
+	socket.on('game:nextPlayer', handleNextPlayer)
  
-	 socket.on('send:ship:sunk:to:opponent', handleSendShipSunk)
+	socket.on('send:ship:sunk:to:opponent', handleSendShipSunk)
 
-	 socket.on('player:has:no:ships:left', handleNoShipsLeft)
+	socket.on('player:has:no:ships:left', handleNoShipsLeft)
 
-	 socket.on("opponent:left", handleOpponentLeft)
- }
+	socket.on("opponent:left", handleOpponentLeft)
+}
